60923f7b81f30be7fcdd843237e10afe6339781c
King-AJr <talk2king.aj@gmail.com>
2022-09-08 04:47:28 -0700

    use printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * This is a simple program that prints a messsage * with puts 
+ * This is a simple program that prints a messsage * with puts
  */
-int main ()
+int main (void)
 {
 puts("\"Programming is like building a multilingual puzzle \n");
 return(0);
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/**
+ * this is a program that prints a message
+ */
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+}
