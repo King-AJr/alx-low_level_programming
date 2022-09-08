5018fa18f3e5b296c841ba040659816002b8398f
King-AJr <talk2king.aj@gmail.com>
2022-09-08 05:36:01 -0700

    please betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(floatType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(lType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(llType));
-printf("Size of a float: %zu byte(s)\n", sizeof(floatType)); 
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
