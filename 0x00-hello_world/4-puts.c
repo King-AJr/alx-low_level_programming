6b5074f059c84273c590e7afd4fb0d774f1f0ac2
King-AJr <talk2king.aj@gmail.com>
2022-09-08 04:09:27 -0700

    adding discription

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * This is a simple program that prints a messsage * with puts 
+ */
 int main ()
 {
-puts("Programming is like building a multilingual puzzle \n");
+puts("\"Programming is like building a multilingual puzzle \n");
 return(0);
 }
