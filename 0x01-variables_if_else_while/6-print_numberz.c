8bfe6940e4e1c922bfe85442a2d0b3e650113f62
King-AJr <talk2king.aj@gmail.com>
2022-09-09 16:19:44 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
-int i;
-char a[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-for (i = 0; i <= 10; i++)
-{
-putchar(a[i]);
-}
+
+int a = 0;
+
+for (; a < 10 ; a++)
+putchar('0' + a);
 putchar('\n');
 return (0);
 }
-
