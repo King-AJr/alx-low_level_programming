ade3beb6337ad093c07bee230f8e024cf550f4b7
King-AJr <talk2king.aj@gmail.com>
2022-09-09 15:58:12 -0700

    pleasing betty

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
+*main - Entry point
 *
-* Return: Always 0 (Success)                      */
-                                                  int main(void)
+*Return: Always 0 (Success)
+*/
+
+int main(void)
 {
 char c = 'z';
 while (c >= 'a')
