9a5b586d948c2dd69418a6d815bfa5b915506940
King-AJr <talk2king.aj@gmail.com>
2022-09-09 16:57:34 -0700

    _

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+#include <unistdio.h>
+
+/**
+  * main - Entry point 
+  *
+  * Return: Always 1 (Success)
+  */
+Int main(void)
+{
+        Write(1, "string\n", nuumber");
+        return (1);
+}
