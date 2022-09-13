08788e91957e282f6fe0da11a3b0ef89a6f2b6ab
King-AJr <talk2king.aj@gmail.com>
2022-09-13 14:20:06 -0700

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,19 +13,28 @@ int rone, cone, d;
 for (rone = 0; rone <= 9; rone++)
 {
 _putchar('0');
-
+_putchar(',');
+_putchar(' ');
 for (cone = 1; cone <= 9; cone++)
+{
+d = (rone * cone);
+if ((d / 10) > 0)
+{
+_putchar((d / 10) + '0');
+}
+else
+{
+_putchar(' ');
+}
+_putchar((d % 10) + '0');
+
+if (cone < 9)
 {
 _putchar(',');
 _putchar(' ');
-d = (rone * cone);
 
-if (cone < 9)
-_putchar(' ');
-
-else 
-_putchar((d / 10) + '0');
 }
 _putchar('\n');
+}
 }
 }
