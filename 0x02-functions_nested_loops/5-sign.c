3aa315eed49e7bfba95569b0dc81e1478c1b9d5d
King-AJr <talk2king.aj@gmail.com>
2022-09-13 14:50:58 -0700

    appeasing betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 #include "main.h"
 
-/*
+/**
  * print_sign - a function that prints the sign of a number
  * @n: contains argument
- * Return - 1 if n>0, 0 if n is 0, -1 if n<0
+ * Return: 1 if n>0, 0 if n is 0, -1 if n<0
  */
 
 int print_sign(int n)
