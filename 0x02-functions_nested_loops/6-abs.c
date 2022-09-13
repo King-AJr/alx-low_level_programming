dcf3cdb02d65cdecd596c94796618624a929af43
King-AJr <talk2king.aj@gmail.com>
2022-09-13 09:19:16 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,31 +3,9 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * return 0
- */
-
-int main(void)
-{
-int r;
-
-r = _abs(-1);
-_putchar(r);
-r = _abs(0);
-_putchar(r);
-r = _abs(1);
-_putchar(r);
-r = _abs(-98);
-_putchar(r);
-return (0);
-}
-
-/**
  *_abs - returns the abs value of an argument
- *@ab - carries argument
- *@a - stores result of computation
- *return value of a
+ *@ab: carries argument
+ *Return: value of a
  */
 
 int _abs(int ab)
