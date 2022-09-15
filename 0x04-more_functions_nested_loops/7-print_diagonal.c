19140ff41ef50bc68adfc682b68830b8e66a9f24
King-AJr <talk2king.aj@gmail.com>
2022-09-15 05:55:00 -0700

    task 7 update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,9 +6,22 @@
  * Return: return void
  */
 
+int main(void)
+{
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
+    return (0);
+}
+
 void print_diagonal(int n)
 {
 int i, j;
+if (n == 0 || n < 0)
+_putchar('\n');
+else
+{
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
@@ -18,5 +31,5 @@ _putchar(' ');
 _putchar('\\');
 _putchar('\n');
 }
-_putchar('\n');
+}
 }
