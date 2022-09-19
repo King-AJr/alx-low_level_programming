2501aac1469dbe94ddaf7832427b1ebd36685b67
King-AJr <talk2king.aj@gmail.com>
2022-09-19 04:51:25 -0700

    prints a given string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+
+
 /* assigns the memory of an int to pointer*/
 void reset_to_98(int *n);
 
@@ -6,3 +8,8 @@ void swap_int(int *a, int *b);
 
 /* returns the length of a given string*/
 int _strlen(char *s);
+
+/* prints a string with the _putchar fx*/
+void _puts(char *str);
+
+
