b3964b712231967027119717ccecd85ae340398a
King-AJr <talk2king.aj@gmail.com>
2022-09-19 07:20:51 -0700

    .

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,7 @@ void puts_half(char *str)
 int length = strlen(str);
 int len = length / 2;
 int i;
-for (i = len - 1; i <= length; i++)
+for (i = len + 1; i <= length; i++)
 {
 _putchar(str[i]);
 }
