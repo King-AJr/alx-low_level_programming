ebda5bf63946167b9cb5bf7eee257492e0cc53ea
King-AJr <talk2king.aj@gmail.com>
2022-09-28 05:21:03 -0700

    task 1 on recursions

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+this repository is for alx task on recursions
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/*putchar function*/
+int _putchar (char c);
+/*
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+*/
+/*this function outputs strings followed by a new line*/
+void _puts_recursion(char *s);
