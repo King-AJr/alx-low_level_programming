3acda841c5a1806788d757136c1164d65178cd29
King-AJr <talk2king.aj@gmail.com>
2022-09-28 05:38:33 -0700

    task 1 - reverse string

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,15 @@
 #include <unistd.h>
 /*putchar function*/
 int _putchar (char c);
-/*
+
 int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
-*/
+
+
 /*this function outputs strings followed by a new line*/
 void _puts_recursion(char *s);
+
+/* print strings in reverse*/
+void _print_rev_recursion(char *s);
