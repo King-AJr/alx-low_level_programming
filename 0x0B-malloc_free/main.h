01bfce1f7c66d0f415ed007f2d23ad1fe8a2fded
King-AJr <talk2king.aj@gmail.com>
2022-10-04 16:48:42 -0700

    task 3

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -13,3 +13,6 @@ char *create_array(unsigned int size, char c);
 
 /*returns a pointer to a given string*/
 char *_strdup(char *str);
+
+/*concatenates 2 strings*/
+char *str_concat(char *s1, char *s2);
