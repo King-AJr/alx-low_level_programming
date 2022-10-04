71d8a3e65d5679e1fbabbc4e60fb01cd31c8087f
King-AJr <talk2king.aj@gmail.com>
2022-10-04 15:30:17 -0700

    task 0

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+include <unistd.h>
+/*putchar function*/
+int _putchar (char c);
+/*
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+*/
+
+/*creates an areay of character and initializes it with a single char*/
+char *create_array(unsigned int size, char c);
+
