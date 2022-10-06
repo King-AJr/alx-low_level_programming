812f63fae2717b144c37259b24108ae9726db0d5
King-AJr <talk2king.aj@gmail.com>
2022-10-06 14:35:20 -0700

    task 2 update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,21 +4,6 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *concat;
-
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
-}
-
-/**
  * string_nconcat - concatenate 2 strings
  * @s1: destination string
  * @s2: source string
