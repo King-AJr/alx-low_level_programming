3c217e8d29ab4708e08cce51cbc324920c4cd30a
King-AJr <talk2king.aj@gmail.com>
2022-10-06 14:33:32 -0700

    task 2 update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
+    return (0);
+}
 
 /**
  * string_nconcat - concatenate 2 strings
@@ -12,22 +28,26 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
+	unsigned int i;
 	int j;
+	int a;
+	char *c;
 
 	j = 0;
 
-	int a = (strlen(s1)) + 1;
+
+       	a = (strlen(s1)) + 1;
 
 	if (s1 == NULL)
 		s1 = " ";
 	if (s2 == NULL)
 		s2 = " ";
-	char *c = malloc(sizeof(char) * (a + n));
+	
+       	c = malloc(sizeof(char) * (a + n));
 
 	if (c == NULL)
 		return (NULL);
-	for (i = 0; i < s1[i]; i++)
+	for (i = 0; s1[i]; i++)
 	{
 		c[j++] = s1[i];
 	}
