1b71e61acf9c3f82ea52c1a0b1000dec8a58e291
King-AJr <talk2king.aj@gmail.com>
2022-10-06 15:22:32 -0700

    task 3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,3 +3,6 @@ void *malloc_checked(unsigned int b);
 
 /*function concatenates 2 strings*/
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/*allocates space for an array*/
+void *_calloc(unsigned int nmemb, unsigned int size);
