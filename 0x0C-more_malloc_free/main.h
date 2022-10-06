c89bc05f137adee26d5de3acccde18b869ea67cd
King-AJr <talk2king.aj@gmail.com>
2022-10-06 15:51:05 -0700

    task 4

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,3 +6,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 /*allocates space for an array*/
 void *_calloc(unsigned int nmemb, unsigned int size);
+
+/*allocate space to array and fill it with integer from min to max*/
+int *array_range(int min, int max);
