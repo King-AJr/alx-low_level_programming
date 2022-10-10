75ab2c147aacb50ffb9ee3d8734d77d17a340455
King-AJr <talk2king.aj@gmail.com>
2022-10-10 12:04:53 -0700

    header file for dog info

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this directory is for alx task on structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - astruct to contain multiple informations on dogs
+ * @name: stores the name of the dog
+ * @owner: stores the name of the owner of the dog
+ * @age: stores the age of the dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
