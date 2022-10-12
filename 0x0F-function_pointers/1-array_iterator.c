30e4fd06f73f142afbee54e2038a591b218aca29
King-AJr <talk2king.aj@gmail.com>
2022-10-12 08:46:38 -0700

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,10 +14,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 
-	unsigned int i;
-
-	for (i = 0; i <= (size - 1); i++)
+	while (size-- > 0)
 	{
-		action(array[i]);
+		action(*array);
+		array++;
 	}
 }
