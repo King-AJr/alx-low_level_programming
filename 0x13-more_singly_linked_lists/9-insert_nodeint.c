ac6f22aef579906b35b9c403867fd56e135fa299
King-AJr <talk2king.aj@gmail.com>
2022-10-25 12:55:50 -0700

    insert node at s specified position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,16 +10,15 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
 	listint_t *new, *copy = *head;
-
-	if (copy == NULL)
-		return (NULL);
+	unsigned int node;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	
+
+	new->n = n;
+
 	if (idx == 0)
 	{
 		new->next = copy;
@@ -27,12 +26,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	new->n = n;
-
-	for (i = 0; i < (idx - 1); i++)
+	for (node = 0; node < (idx - 1); node++)
 	{
-		if (copy == NULL)
+		if (copy == NULL || copy->next == NULL)
 			return (NULL);
+
 		copy = copy->next;
 	}
 
