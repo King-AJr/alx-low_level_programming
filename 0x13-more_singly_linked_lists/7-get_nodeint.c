a7309b8ea5aa1713afaf74855df49ab4be8a4c57
King-AJr <talk2king.aj@gmail.com>
2022-10-25 12:07:56 -0700

    fetch node with specified index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,14 +11,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	int i;
 
-	i = 0;
 	for (i = 0; i < index; i++)
 	{
+		if (head == NULL)
+			return (NULL);
+
 		head = head->next;
 	}
-
-	if (head == NULL)
-		return (NULL);
 
 	return (head);
 }
