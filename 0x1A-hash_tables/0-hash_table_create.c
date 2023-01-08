3ad14822be6171ecf88c4b98591e426ea89d8069
King-AJr <talk2king.aj@gmail.com>
2023-01-08 08:12:02 -0800

    add description and correct indentation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
+ */
+
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -9,11 +17,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
-		return NULL;
+		return (NULL);
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t) * size);
 	if (table->array == NULL)
-		return NULL;
+		return (NULL);
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
